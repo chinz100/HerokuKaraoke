a2d2f371e158f00ff4ab97cc2397fbaa27708ac7
chinz100 <chinz0020@gmail.com>
2019-08-22 01:50:22 +0700

    make it better

--- index.js
+++ index.js
@@ -12,52 +12,6 @@ app.listen(port, () => {
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-const database = new Datastore('database.db');
-database.loadDatabase();
-
-app.get('/api', (request, response) => {
-  database.find({}, (err, data) => {
-    if (err) {
-      response.end();
-      return;
-    }
-    response.json(data);
-  });
-});
 var karaoke = require('./karaoke')
 app.use('/newuser', karaoke.thai2karaoke());
-app.post('/api', (request, response) => {
-  const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
-  response.json(data);
-});
-/*
-googleTranslate.translate('My name is Brandon', 'es', function(err, translation) {
-  console.log(translation.translatedText);
-  // =>  Mi nombre es Brandon
-});
-*/
-app.get('/weather/:latlon', async (request, response) => {
-  console.log(request.params);
-  const latlon = request.params.latlon.split(',');
-  console.log(latlon);
-  const lat = latlon[0];
-  const lon = latlon[1];
-  console.log(lat, lon);
-  const api_key = process.env.API_KEY;
-  const weather_url = `https://api.darksky.net/forecast/${api_key}/${lat},${lon}/?units=si`;
-  const weather_response = await fetch(weather_url);
-  const weather_data = await weather_response.json();
 
-  const aq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`;
-  const aq_response = await fetch(aq_url);
-  const aq_data = await aq_response.json();
-
-  const data = {
-    weather: weather_data,
-    air_quality: aq_data
-  };
-  response.json(data);
-});
