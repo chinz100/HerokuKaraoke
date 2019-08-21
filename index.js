6b912d5404cccda9d58abb37a08c937870900705
chinz100 <chinz0020@gmail.com>
2019-08-22 01:53:43 +0700

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const Datastore = require('nedb');
+const Datastore = require('nedb');
 //const fetch = require('node-fetch');
 //var googleTranslate = require('google-translate')(apiKey);
 require('dotenv').config();
